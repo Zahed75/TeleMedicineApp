88d8336f560bbea2e97efae07079cba12c63df50
tasincodes <md.shahrier.tasin@g.bracu.ac.bd>
2024-03-18 10:38:24 +0600

    update od add testresult

--- src/routes/index.js
+++ src/routes/index.js
@@ -5,21 +5,13 @@ const router = express.Router();
 const authVerifyMiddleware = require("../middlewares/authMiddleware.js");
 
 //routes
-<<<<<<< HEAD
- const authRoute = require('../modules/Auth/controller');
- const viewTestresultroute = require('../modules/viewTestresult/controller.js')
-
+const authRoute = require('../modules/Auth/controller');
+const viewTestresultroute = require('../modules/viewTestresult/controller.js');
+const medicationRoute = require("../modules/MedicationRecord/controller");
 
 // Root End Point
 router.use('/auth', authRoute);
 router.use('/viewTestresult', viewTestresultroute);
-=======
-const authRoute = require("../modules/Auth/controller");
-const medicationRoute = require("../modules/MedicationRecord/controller");
-
-// Root End Point
-router.use("/auth", authRoute);
 router.use("/medication", medicationRoute);
->>>>>>> 84cbfc13cc6c88ce43338a4b4f186f1fc3925924
 
 module.exports = router;

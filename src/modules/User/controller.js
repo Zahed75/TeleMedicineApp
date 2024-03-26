1bb89f82b345868d5c879a6696d063748d473c41
shaiadul <devshaiadul69@gmail.com>
2024-03-26 12:29:32 +0600

    pushing

--- src/modules/User/controller.js
+++ src/modules/User/controller.js
@@ -1,16 +1,27 @@
 const express = require("express");
 const router = express.Router();
 
-const { searchDoctorsByNames } = require("./service");
-const {getUsers}= require('./service')
+const { getUsers, searchDoctorsByNames } = require("./service");
 
+const getAllUser = async (req, res) => {
+  try {
+    const allUser = await getUsers(req.query.limit, req.query.skip);
+    if (!allUser) {
+      res.status(401).json({ message: "User not found" });
+    }
+    res.status(200).json(allUser);
+  } catch (error) {
+    console.error("Error getting User", error);
+    res.status(500).json({ success: false, error: "Internal server error" });
+  }
+};
 
 const searchByDoctorName = async (req, res) => {
   try {
-    const doctors = await searchDoctorsByNames(req.query);
+    const doctors = await searchDoctorsByNames(req.query.userName.split(","));
     res
       .status(200)
-      .json({ success: true, data: doctors, count: doctors.length });
+      .json({ success: true, data: doctors, count: doctors.length, message: "Doctors found" });
   } catch (error) {
     console.error("Error getting all doctors:", error);
     res.status(500).json({ success: false, error: "Internal server error" });
@@ -18,28 +29,8 @@ const searchByDoctorName = async (req, res) => {
 };
 
 
-const getAllUser= async(req,res) =>{
 
-  try{
-    const allUser = await getUsers(
-      req.query.limit,
-      req.query.skip
-    )
-    if(!allUser){
-      res.status(401).json({message:"User not found"})
-    } 
-    res.status(200).json(allUser)
-
-  }
-  catch (error) {
-    console.error("Error getting User", error);
-    res.status(500).json({ success: false, error: "Internal server error" });
-  }
-
-  
-}
-
-router.get("/search", searchByDoctorName);
 router.get("/getAllUser", getAllUser);
+router.get("/search", searchByDoctorName);
 
 module.exports = router;
--- src/modules/User/service.js
+++ src/modules/User/service.js
@@ -1,193 +1,36 @@
-const jwt = require('jsonwebtoken');
+const UserModel = require("./model");
 
-const UserModel = require('./model');
-
-const {
-  sendSetPasswordEmail,
-  setPasswordEmailOutlet,
-} = require('../../utility/email');
-const { NotFound, BadRequest, Forbidden } = require('../../utility/errors');
-// const { BASIC_USER,
-// CELEBRITY_VIP,
-// CHRUCH_LEADER,
-// CHRUCH_PAGE,
-// SUPER_ADMIN,
-//  } = require('../../config/constant');
-const { generateOTP } = require('../../utility/common');
-const { SendEmailUtility } = require('../../utility/email');
-const { all } = require('axios');
-
-const addUser = async (user) => {
-  const newUser = await User.create(user);
-  return newUser;
-};
-
-
-
-const updateUser = async (userId, updatedValue) => {
-  const updatedUser = await User.findByIdAndUpdate(
-    {
-      _id: userId,
-    },
-    updatedValue,
-    {
-      new: true,
-    }
-  );
-
-  if (!updateUser) {
-    throw new NotFound('User not found');
-  }
-
-  await Brand.updateMany(
-    {
-      ownerId: userId,
-    },
-    {
-      ownerName: updatedUser.name,
-    }
-  );
-
-  return updatedUser;
-};
-
-
-
-const deleteUserById = (userId) => {};
-
-const getUsers = (
-  limit,
-  skip
-) => {
+const getUsers = (limit, skip) => {
   return UserModel.find().limit(limit).skip(skip);
 };
 
-const getUserById = async (id) => {
-  const user = await User.findById({ _id: id });
-
-  if (!user) throw new NotFound('User not found');
-
-  return user;
-};
-
-
-
-const updateEmail = async (id, values) => {
-  const { changedEmail } = values;
-  const isUser = await User.findById({ _id: id });
-  if (!isUser) throw new NotFound('User not found');
-
-  const otp = generateOTP();
-
-  isUser.emailChangeOTP = otp;
-  isUser.changedEmail = changedEmail;
-
-  isUser.emailChangeOTP = otp;
-  isUser.changedEmail = changedEmail;
-
-  await isUser.save();
-
-  const emailBody = `Verification OTP: ${otp}`;
-  await SendEmailUtility(changedEmail, emailBody, 'OTP');
-
-  return isUser;
-};
-
-
-
-
-const verifyChangedEmail = async (id, otp) => {
-  const isUser = await User.findById({ _id: id });
-
-  if (!isUser) throw new NotFound('User not found');
-
-  if (isUser.emailChangeOTP !== Number(otp))
-    throw new BadRequest('Invalid OTP');
-
-  const updatedUser = await User.findByIdAndUpdate(
-    { _id: id },
-    {
-      $set: {
-        email: isUser.changedEmail,
-      },
-      $unset: {
-        changedEmail: 1,
-        emailChangeOTP: 1,
-      },
-    },
-    { new: true }
-  ).select('name phoneNumber email password isActive isVerified role brands');
-
-  return updatedUser;
-};
-
-
-
-// Admin Set Password
-const updatePassword = async (id, body) => {
-  const { currentPassword, newPassword, confirmPassword } = body;
-  const user = await User.findById({ _id: id });
-  if (!user) throw new NotFound('User not found');
-
-  const isPassword = await user.authenticate(currentPassword);
-
-  if (!isPassword) throw new BadRequest('Current password did not match');
-
-  if (newPassword !== confirmPassword)
-    throw new BadRequest('New password does not match');
-
-  user.password = newPassword;
-
-  await user.save();
-
-  return user;
-};
-
-
-
-
-
-//for resending reset password email to managers(brand/outlet)
-const resendResetPasswordEmail = async (managerId) => {
-  const user = await User.findById({ _id: managerId });
-  if (!user) throw new NotFound('User not found');
-
-  if (user.role === BRAND_MANAGER) {
-    await sendSetPasswordEmail(user.email, user);
-  } else if (user.role === OUTLET_MANAGER) {
-    await setPasswordEmailOutlet(user.email, user);
-  }
-
-  return user;
-};
-
-
-
-
-const searchDoctorsByNames = async (userName) => {
+const searchDoctorsByNames = async (userName) => { 
   try {
-    const doctors = await User.find({
+    const doctors = await UserModel.find({
       userName: { $in: userName },
-      role: DOCTOR,
-    });
+      role: "DC",
+    }).select({ userName: 1, email: 1, role: 1 });
     return doctors;
   } catch (error) {
     throw error;
   }
 };
 
-
+// const searchDoctorsByNames = async (req, res) => {
+//   const { userName } = req.query;
+//   try {
+//     const doctors = await UserModel.find({
+//       userName: { $in: userName.split(",") },
+//       role: "DC",
+//     }).select({ userName: 1, email: 1, role: 1 });
+//     return doctors;
+//   } catch (error) {
+//     throw error;
+// }
+// }
 
 
 module.exports = {
-  addUser,
-  updateUser,
-  deleteUserById,
   getUsers,
-  getUserById,
-  updateEmail,
-  verifyChangedEmail,
-  updatePassword,
-  resendResetPasswordEmail,
   searchDoctorsByNames,
 };
